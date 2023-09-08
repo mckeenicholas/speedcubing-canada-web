4f834df05ec2523513b85694b0283e35709bab51
Nicholas McKee <nicholas.mckee@mail.utoronto.ca>
2023-09-08 14:54:34 -0400

    refactor using prettier

--- app/src/pages/Competitions.tsx
+++ app/src/pages/Competitions.tsx
@@ -1,27 +1,42 @@
-import { Button, Box, Container, Typography, SelectChangeEvent, OutlinedInput, MenuItem, Select, FormControl, InputLabel, CircularProgress, InputAdornment, IconButton, Tooltip, } from "@mui/material";
+import {
+  Button,
+  Box,
+  Container,
+  Typography,
+  SelectChangeEvent,
+  OutlinedInput,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  CircularProgress,
+  InputAdornment,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { Link } from "../components/Link";
 import { LINKS } from "./links";
 import { useState, useEffect } from "react";
-import { LocationOn, ArrowForward } from '@mui/icons-material';
+import { LocationOn, ArrowForward } from "@mui/icons-material";
 
 export const Competitions = () => {
   const { t } = useTranslation();
 
   const [competitionList, setCompetitionList] = useState<any>({});
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
   const [distance, setDistance] = useState<number>(0);
-  const [postalCode, setPostalCode] = useState('');
+  const [postalCode, setPostalCode] = useState("");
   const [locationInfo, setLocationInfo] = useState<any>({});
   const [filteredComps, setFilteredComps] = useState<any[]>([]);
   const [invalidPostalCode, setInvalidPostalCode] = useState(false);
   const [invalidPostalCodeName, setInvalidPostalCodeName] = useState("");
-  
+
   // Get a list of announced competitions within Canada
   const getCompetitions = async () => {
     const response = await fetch(LINKS.WCA.API.COMPETITION_LIST);
     return await response.json();
-  }
+  };
 
   // Hook to update the competitions displayed on the site, and filter out any past competitions
   useEffect(() => {
@@ -37,7 +52,7 @@ export const Competitions = () => {
       setFilteredComps(filteredCompetitions);
     };
     getCompetitionList();
-  }, []); 
+  }, []);
 
   // Event handler for distance dropdown
   const handleChange = (event: SelectChangeEvent) => {
@@ -46,23 +61,29 @@ export const Competitions = () => {
 
   // Event handler for postal code input field
   const handleInputChange = (event: any) => {
-    setPostalCode(event.target.value); 
+    setPostalCode(event.target.value);
   };
 
   // Handle if the user selects location automatically
   const handleGetLocation = () => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        await reverseGeocode(position.coords.latitude, position.coords.longitude);
-        setLocationInfo({
-          name: postalCode,
-          lat: Number(position.coords.latitude),
-          lon: Number(position.coords.longitude),
-        })
-      }, (error) => {
-        console.error("Error getting user's location:", error.message);
-        // Handle the error gracefully if needed
-      });
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          await reverseGeocode(
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setLocationInfo({
+            name: postalCode,
+            lat: Number(position.coords.latitude),
+            lon: Number(position.coords.longitude),
+          });
+        },
+        (error) => {
+          console.error("Error getting user's location:", error.message);
+          // Handle the error gracefully if needed
+        },
+      );
     } else {
       console.error("Geolocation is not supported in this browser.");
       // Handle the lack of geolocation support gracefully if needed
@@ -70,33 +91,37 @@ export const Competitions = () => {
   };
 
   // Find postal code to display if user selects locaiton automatically
-  const reverseGeocode = async (latitude: number, longitude:number) => {
+  const reverseGeocode = async (latitude: number, longitude: number) => {
     const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&addressdetails=1&format=jsonv2`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     setLocationInfo(data);
     setPostalCode(data.address.postcode);
-  }
+  };
 
   // Find coordinates from postal code
   const geocode = async (postCode: string) => {
-      const apiUrl = `https://nominatim.openstreetmap.org/search?postalcode=${postalCode}&format=json`;
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      if (data === undefined || data.length === 0) {
-        setInvalidPostalCode(true);
-        setInvalidPostalCodeName(postCode);
-        return null;
-      }
-      setInvalidPostalCode(false);
-      setLocationInfo(data[0]);
-      return data[0];
+    const apiUrl = `https://nominatim.openstreetmap.org/search?postalcode=${postalCode}&format=json`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data === undefined || data.length === 0) {
+      setInvalidPostalCode(true);
+      setInvalidPostalCodeName(postCode);
+      return null;
+    }
+    setInvalidPostalCode(false);
+    setLocationInfo(data[0]);
+    return data[0];
   };
 
   // Event handler for the search button, finds the user's coordinates based off their inputted postal code, then filters out competitions
   const handleButtonClick = async (event: any) => {
     let location = locationInfo;
-    if(distance !== 0 && removeWhitespaceAndCase(locationInfo.name) !== removeWhitespaceAndCase(postalCode)) {
+    if (
+      distance !== 0 &&
+      removeWhitespaceAndCase(locationInfo.name) !==
+        removeWhitespaceAndCase(postalCode)
+    ) {
       location = await geocode(postalCode);
     }
     let displayedComps = [];
@@ -107,42 +132,55 @@ export const Competitions = () => {
       });
       setFilteredComps(displayedComps);
       setInvalidPostalCode(false);
-      return
+      return;
     } else if (location !== null) {
       for (const competition of competitionList) {
-        if (findDistance(competition.latitude_degrees, competition.longitude_degrees, location.lat, location.lon) < Number(distance) && new Date(competition.end_date + "T12:00:00.000Z") > new Date()) {
+        if (
+          findDistance(
+            competition.latitude_degrees,
+            competition.longitude_degrees,
+            location.lat,
+            location.lon,
+          ) < Number(distance) &&
+          new Date(competition.end_date + "T12:00:00.000Z") > new Date()
+        ) {
           displayedComps.push(competition);
         }
       }
     }
     setFilteredComps(displayedComps);
-  }
- 
+  };
+
   const removeWhitespaceAndCase = (input: string) => {
     if (input === undefined) {
-      return '';
+      return "";
     }
-    return input.replace(/\s+/g, '').toLowerCase();
-  }
-  
+    return input.replace(/\s+/g, "").toLowerCase();
+  };
+
   // Find the distance in km between two sets of coordinates using Haversine formula
-  const findDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+  const findDistance = (
+    lat1: number,
+    lon1: number,
+    lat2: number,
+    lon2: number,
+  ) => {
     const earthRadius = 6371;
 
     const radLat1 = (Math.PI / 180) * lat1;
     const radLon1 = (Math.PI / 180) * lon1;
     const radLat2 = (Math.PI / 180) * lat2;
     const radLon2 = (Math.PI / 180) * lon2;
-  
+
     const dLat = radLat2 - radLat1;
     const dLon = radLon2 - radLon1;
     const a =
       Math.sin(dLat / 2) ** 2 +
       Math.cos(radLat1) * Math.cos(radLat2) * Math.sin(dLon / 2) ** 2;
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  
+
     return earthRadius * c;
-  }
+  };
 
   return (
     <Container maxWidth="xl" style={{ textAlign: "center" }}>
@@ -150,14 +188,21 @@ export const Competitions = () => {
         <Typography component="h1" variant="h3" fontWeight="bold" gutterBottom>
           {t("competition.upcoming")}
         </Typography>
-        <Typography gutterBottom  sx={{ maxWidth: "md", margin: "0 auto"}}>
+        <Typography gutterBottom sx={{ maxWidth: "md", margin: "0 auto" }}>
           {t("competition.upcomingbody")}
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px' }}>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "10px",
+            }}
+          >
             {t("competition.showonly1")}
-            <FormControl sx={{ width: '15ch' }}>
-              <InputLabel sx={{ textAlign: 'left' }} id="distance-selection">
+            <FormControl sx={{ width: "15ch" }}>
+              <InputLabel sx={{ textAlign: "left" }} id="distance-selection">
                 {t("competition.distance")}
-                </InputLabel>
+              </InputLabel>
               <Select
                 labelId="distance-selection"
                 id="distance-selection"
@@ -175,8 +220,10 @@ export const Competitions = () => {
               </Select>
             </FormControl>
             {t("competition.showonly2")}
-            <FormControl sx={{ width: '15ch' }}>
-              <InputLabel htmlFor="postal-code-box">{t("competition.postalcode")}</InputLabel>
+            <FormControl sx={{ width: "15ch" }}>
+              <InputLabel htmlFor="postal-code-box">
+                {t("competition.postalcode")}
+              </InputLabel>
               <OutlinedInput
                 error={invalidPostalCode}
                 value={postalCode}
@@ -197,37 +244,52 @@ export const Competitions = () => {
                 label="Postal Code"
               />
             </FormControl>
-            <Button onClick={handleButtonClick} variant="contained" size="large">
+            <Button
+              onClick={handleButtonClick}
+              variant="contained"
+              size="large"
+            >
               <ArrowForward />
             </Button>
           </Box>
         </Typography>
-      </Box> 
+      </Box>
       {isLoading ? (
         <Box margin="4rem">
           <CircularProgress />
         </Box>
       ) : invalidPostalCode ? (
         <Typography gutterBottom sx={{ maxWidth: "md", margin: "0 auto" }}>
-          {t("competition.notfound", {postalcode: invalidPostalCodeName})}
+          {t("competition.notfound", { postalcode: invalidPostalCodeName })}
         </Typography>
       ) : (
         <Box display="flex" justifyContent="center" flexWrap="wrap">
-          {filteredComps.slice().reverse().map((item: any, index: any) => (
-            <Box margin="1rem" padding="1rem" key={index}>
-              <Typography variant="h5" fontWeight="bold">
-                {item.name}
-              </Typography>
-              <Typography gutterBottom maxWidth="400px">
-                {new Date(item.start_date + "T12:00:00.000Z").toLocaleString("en-US", { month: "long" })}
-                {" | "}
-                {item.city}
-              </Typography>
-              <Button to={`competitions/${item.id}`} component={Link} variant="contained" size="large">
-                {t("competition.learnmore")}
-              </Button>
-            </Box>
-          ))}
+          {filteredComps
+            .slice()
+            .reverse()
+            .map((item: any, index: any) => (
+              <Box margin="1rem" padding="1rem" key={index}>
+                <Typography variant="h5" fontWeight="bold">
+                  {item.name}
+                </Typography>
+                <Typography gutterBottom maxWidth="400px">
+                  {new Date(item.start_date + "T12:00:00.000Z").toLocaleString(
+                    "en-US",
+                    { month: "long" },
+                  )}
+                  {" | "}
+                  {item.city}
+                </Typography>
+                <Button
+                  to={`competitions/${item.id}`}
+                  component={Link}
+                  variant="contained"
+                  size="large"
+                >
+                  {t("competition.learnmore")}
+                </Button>
+              </Box>
+            ))}
         </Box>
       )}
     </Container>
